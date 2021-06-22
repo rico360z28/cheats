f9ab0c2001c0a477327caeb31a63089f61f1c26f
Rico Cantrell <ricocantrell@gmail.com>
2021-06-22 15:26:00 -0700

    Add files via upload
    used to track connection issues along with cronjob

---
+++ netlog.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "##Begin##" >>/home/rico-admin/Documents/results
+echo "##When am I?##" >>/home/rico-admin/Documents/results
+date >>/home/rico-admin/Documents/results
+echo "##Wrere am I?##" >>/home/rico-admin/Documents/results
+curl ifconfig.me >>/home/rico-admin/Documents/results
+echo "" >>/home/rico-admin/Documents/results
+echo "##IPs of machine##" >>/home/rico-admin/Documents/results
+ip a | grep inet >>/home/rico-admin/Documents/results
+echo "##Routes up##?" >>/home/rico-admin/Documents/results
+route >>/home/rico-admin/Documents/results
+echo "##Hosts up?##" >>/home/rico-admin/Documents/results
+ping -c 5 192.168.10.1 -s 1500 -M do >>/home/rico-admin/Documents/results
+ping -c 5 192.168.10.4 -s 4096 -M do >>/home/rico-admin/Documents/results
+echo "##Services##" >>/home/rico-admin/Documents/results
+netstat -at | grep ssh >>/home/rico-admin/Documents/results
+echo "##DNS##" >>/home/rico-admin/Documents/results
+nslookup rico.com 8.8.8.8 >>/home/rico-admin/Documents/results
+echo "##End##" >>/home/rico-admin/Documents/results
+echo "" >>/home/rico-admin/Documents/results
