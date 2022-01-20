878f6210bc4dacca1c4cecf3c3308dd74fab34d0
Rico Cantrell <ricocantrell@gmail.com>
2022-01-20 11:39:37 -0700

    Update certsNservices.sh

--- certsNservices.sh
+++ certsNservices.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
+sleep 10
 systemctl stop meshcentral.service
+sleep 5
 systemctl stop unifi
+sleep 5
 /usr/bin/certbot renew --quiet
 /root/unifi_ssl_import.sh
 # https://github.com/stevejenkins/ubnt-linux-utils/
 kill $(ps aux | grep '[n]ginx' | awk '{print $2}')
-systemctl restart meshcentral.service
-systemctl restart unifi
+sleep 5
+systemctl restart meshcentral.service &
+systemctl restart unifi &
+exit 0
